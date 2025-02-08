9f2c54b2bcc415c660320af8e8fe8055343ae8d5
apclccruz <lccruz@student.apc.edu.ph>
2025-02-08 22:04:32 +0800

    Update

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -5,7 +5,7 @@
           <section class="main-content">
             <div>1 About Me: Hello! I'm Latrell Allen Cruz, and I am currently a 3rd-year student pursuing a BSIT.</div>
             <div>2 Education / Achievements: I graduated from Pateros Catholic School and went on to study CSIT. I'm passionate about web development and currently learning backend technologies like Java.</div>
-            <div>3 Course: Currently studying BSc. CSIT at my university and working towards gaining deeper knowledge in backend development. </div>  
+            <div>3 Course: Currently studying BSc. CSIT at my university and working towards gaining deeper knowledge in backend development.</div>  
             <div>4 IT Experience: I've learned programming languages like Python and Java, and I have experience in frontend technologies such as HTML and CSS.</div>
             <div>5 Hobbies & Interests: I love learning new technologies, coding, and solving problems. In my free time, I enjoy playing basketball with my family.</div>
             <div>6 Goals In Life / Dream: My goal is to give back to my parents for their sacrifices and make my family proud by excelling in the field of IT.</div>  
